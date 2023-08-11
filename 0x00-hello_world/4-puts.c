b0ad6bcb6b3e852ae5da4158ee044bb24ea7a7a9
israamaher <esraamaher770@gmail.com>
2023-08-10 18:17:05 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-	char str[]="\"Programming is like building a multilingual puzzle";
+	//string initialisation
+	char str[ ] = "\"Programming is like building a multilingual puzzle";
 
 	puts(str);	
     return (0);
