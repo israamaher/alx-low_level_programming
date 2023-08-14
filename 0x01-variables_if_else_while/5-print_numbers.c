2a7658234ff693a0306313b0da88e22b8ecb8fc9
israamaher <esraamaher770@gmail.com>
2023-08-13 19:59:52 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	int number;
+
+	for (number = 0 ; number < 10 ; number++)
+		printf("%i", number);
+	putchar('\n');
+	return (0);
+}	
