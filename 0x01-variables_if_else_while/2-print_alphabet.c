d9d0fc4e69dbd92336dee7a3195d87b4e7ac2980
israamaher <esraamaher770@gmail.com>
2023-08-13 18:35:56 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - I sometimes suffer from insomnia. And when I can't fall asleep,
+ *  I play what I call the alphabet game
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	char alphabet;
+	
+	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		putchar(alphabet);
+	
+	putchar('\n');
+
+	 return (0);
+}
