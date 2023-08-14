1fed3beb60ff9c8ddedd4f5fce0835b5aded3ce8
israamaher <esraamaher770@gmail.com>
2023-08-14 16:43:56 -0700

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Numbers
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	char number = '0';
+
+	for (number = '0' ; number <= '9'; number++)
+		putchar(number);
+	putchar('\n');
+	return (0);
+}
