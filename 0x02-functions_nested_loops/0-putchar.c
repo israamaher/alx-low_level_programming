0d8738099baae52707d04fd8f1a08ffaeb1a990d
israamaher <esraamaher770@gmail.com>
2023-08-15 18:10:08 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - putchar
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+}
