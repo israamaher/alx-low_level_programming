69ace828fa201038d45c910029491b5aa272c763
israamaher <esraamaher770@gmail.com>
2023-08-16 18:27:12 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - I sometimes suffer from insomnia.
+ *  And when I can't fall asleep,
+ *  I play what I call the alphabet game
+ * this program is compiled on.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+ void print_alphabet(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
+		_putchar(alphabet);
+	_putchar('\n');
+}
+
