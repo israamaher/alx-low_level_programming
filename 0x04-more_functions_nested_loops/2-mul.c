c93ad67ada25d771883786c6bef2c4341b6e3c4f
israamaher <esraamaher770@gmail.com>
2023-08-19 08:31:06 -0700

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <ctype.h>
+#include <stdio.h>
+/**
+ * _mul - check if character is lowercase.
+ *@a: is integer to  multiple 
+ @b: is integer to  multiple
+ * Return: 0 if succes
+ **/
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	printf("%i",mul);
+	return (0);
+}
