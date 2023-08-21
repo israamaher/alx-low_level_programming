9b443e5dbc581dfb448229d4da862d5b49ebb562
israamaher <esraamaher770@gmail.com>
2023-08-20 17:23:24 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers
+ * Return: the result of the operation
+ */
+void print_numbers(void)
+{
+	char i;
+
+	for (i = 0; i < 10; i++)
+		_putchar('0' + i);
+	_putchar('\n');
+}
