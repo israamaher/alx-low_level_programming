96e46cdc4191e5ba0406ae0ff2d71f2b652b0231
israamaher <esraamaher770@gmail.com>
2023-08-21 17:12:53 -0700

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/*
+ * _puts - prints a string to stdout
+ * @str: string
+ **/
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
