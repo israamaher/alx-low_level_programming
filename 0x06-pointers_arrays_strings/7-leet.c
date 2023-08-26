26b3c7a874e6854c71fa4a99faba6bcd7f38f926
israamaher <esraamaher770@gmail.com>
2023-08-26 16:11:19 -0700

    7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - Write a function that encodes a string into 1337.
+ * @str : input value
+ * Return: n value
+ **/
+char *leet(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 'A' || str[i] == 'a')
+			str[i] = '4';
+		else if (str[i] == 'E' || str[i] == 'e')
+			str[i] = '3';
+		else if (str[i] == 'O' || str[i] == 'o')
+			str[i] = '0';
+		else if (str[i] == 'T' || str[i] == 't')
+			str[i] = '7';
+		else if (str[i] == 'L' || str[i] == 'l')
+			str[i] = '1';
+	}
+	return (str);
+}
