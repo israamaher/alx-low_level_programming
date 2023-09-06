01be8e53c0937150e794d2c2b62d3312c335a668
israamaher <esraamaher770@gmail.com>
2023-09-05 23:03:22 -0700

    3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,8 +11,29 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j;
+	int i, j, **arr;
 
+	if (width <= 0 || height <= 0)
+		return (0);
 
+	arr = malloc(sizeof(int) * height);
 
+	if (arr == 0)
+		return (0);
+
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(sizeof(int) * width);
+
+		if (arr[i] == 0)
+		{
+			while (i--)
+				free(arr[i]);
+			free(arr);
+			return (0);
+		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
 }
