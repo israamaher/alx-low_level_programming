84f050162489025c7d339ceefaccba1fe0c6c17d
israamaher <esraamaher770@gmail.com>
2023-09-14 08:19:16 -0700

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,24 +13,19 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *str;
 	va_list pt;
 
-
-	if (separator == NULL)
-	{
-		printf("\n");
-		return;
-	}
 	va_start(pt, n);
 
 	for (i = 0; i < n; i++)
 	{
 		str = va_arg(pt, char *);
-		if (i == 0)
-			printf("%s", str);
-		else
-		{
+
+		if (separator != NULL && i > 0)
 			printf("%s", separator);
+		if (str == NULL)
+			printf("(nil)");
+		else
 			printf("%s", str);
-		}
+
 	}
 	va_end(pt);
 	printf("\n");
